17f9fc0fae54ae095534dafc33d6a96a15386a0f
selfimprover321 <136753822+selfimprover321@users.noreply.github.com>
2024-08-20 16:21:32 -0400

    Exercise 1.5

--- courseInfo/src/App.jsx
+++ courseInfo/src/App.jsx
@@ -33,6 +33,7 @@ const Content = ({parts}) => {
     <div>
       <Part part={parts[0].name} exercises={parts[0].exercises} />
       <Part part={parts[1].name} exercises={parts[1].exercises} />
+     
       <Part part={parts[2].name} exercises={parts[2].exercises} />
     </div>
   )
@@ -41,26 +42,28 @@ const Content = ({parts}) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
